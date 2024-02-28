2a2419ca5720367f96d9fbaa49caefe8447ab44f
Tiziano Marcos Masiello <tizianomasiello1@gmail.com>
2024-02-28 15:23:36 -0300

    intentando solucionar un error

--- js/carrito.js
+++ js/carrito.js
@@ -1,158 +1,158 @@
-import { productosDisponibles } from "./inicio.js";
+// import { productosDisponibles } from "./inicio.js";
 
-JSON.parse(sessionStorage.getItem("carrito")) === null &&
-  sessionStorage.setItem("carrito", JSON.stringify([]));
+// JSON.parse(sessionStorage.getItem("carrito")) === null &&
+//   sessionStorage.setItem("carrito", JSON.stringify([]));
 
-document.addEventListener("DOMContentLoaded", () => {
-  hacerCarrito();
-});
-let carrito = JSON.parse(sessionStorage.getItem("carrito"));
-const listaCarrito = document.getElementById("items");
-const footCarrito = document.getElementById("totales");
-const btnCarrito = document.getElementById("btnCarrito");
+// document.addEventListener("DOMContentLoaded", () => {
+//   hacerCarrito();
+// });
+// let carrito = JSON.parse(sessionStorage.getItem("carrito"));
+// const listaCarrito = document.getElementById("items");
+// const footCarrito = document.getElementById("totales");
+// const btnCarrito = document.getElementById("btnCarrito");
 
-const carritoTabla = document.getElementById("carrito");
+// const carritoTabla = document.getElementById("carrito");
 
-btnCarrito.addEventListener("click", () => {
-  if (carritoTabla.style.display === "block") {
-    carritoTabla.style.display = "none";
-  } else {
-    carritoTabla.style.display = "block";
-  }
-});
+// btnCarrito.addEventListener("click", () => {
+//   if (carritoTabla.style.display === "block") {
+//     carritoTabla.style.display = "none";
+//   } else {
+//     carritoTabla.style.display = "block";
+//   }
+// });
 
-export const comprarProducto = (idProducto) => {
-  const producto = productosDisponibles.find(
-    (producto) => producto.id === idProducto
-  );
+// export const comprarProducto = (idProducto) => {
+//   const producto = productosDisponibles.find(
+//     (producto) => producto.id === idProducto
+//   );
 
-  const { nombre, precio, imagen, id } = producto;
+//   const { nombre, precio, imagen, id } = producto;
 
-  const productoCarrito = carrito.find(
-    (producto) => producto.id === idProducto
-  );
-  if (productoCarrito === undefined) {
-    const nuevoProductoCarrito = {
-      id: id,
-      nombre: nombre,
-      precio: precio,
-      imagen: imagen,
-      cantidad: 1,
-    };
+//   const productoCarrito = carrito.find(
+//     (producto) => producto.id === idProducto
+//   );
+//   if (productoCarrito === undefined) {
+//     const nuevoProductoCarrito = {
+//       id: id,
+//       nombre: nombre,
+//       precio: precio,
+//       imagen: imagen,
+//       cantidad: 1,
+//     };
 
-    carrito.push(nuevoProductoCarrito);
-    sessionStorage.setItem("carrito", JSON.stringify(carrito));
-  } else {
-    const indexProductoCarrito = carrito.findIndex(
-      (producto) => producto.id === idProducto
-    );
-    carrito[indexProductoCarrito].cantidad++;
-    carrito[indexProductoCarrito].precio =
-      precio * carrito[indexProductoCarrito].cantidad;
+//     carrito.push(nuevoProductoCarrito);
+//     sessionStorage.setItem("carrito", JSON.stringify(carrito));
+//   } else {
+//     const indexProductoCarrito = carrito.findIndex(
+//       (producto) => producto.id === idProducto
+//     );
+//     carrito[indexProductoCarrito].cantidad++;
+//     carrito[indexProductoCarrito].precio =
+//       precio * carrito[indexProductoCarrito].cantidad;
 
-    sessionStorage.setItem("carrito", JSON.stringify(carrito));
-  }
-  carrito = JSON.parse(sessionStorage.getItem("carrito"));
-  alert(`Usted ha comprado el producto: ${nombre}`);
-};
+//     sessionStorage.setItem("carrito", JSON.stringify(carrito));
+//   }
+//   carrito = JSON.parse(sessionStorage.getItem("carrito"));
+//   alert(`Usted ha comprado el producto: ${nombre}`);
+// };
 
-const hacerCarrito = () => {
-  listaCarrito.innerHTML = "";
-  carrito.forEach((producto) => {
-    const { imagen, nombre, cantidad, precio, id } = producto;
+// const hacerCarrito = () => {
+//   listaCarrito.innerHTML = "";
+//   carrito.forEach((producto) => {
+//     const { imagen, nombre, cantidad, precio, id } = producto;
 
-    let body = document.createElement("tr");
-    body.className = "producto__carrito";
-    body.innerHTML = `
-    <th><img id="fotoProductoCarrito" src="${imagen}" class="card-image-top" style="width:100%; "</th>
-    <td>${nombre}</td>
-    <td>${cantidad}</td>
-    <td>${precio / cantidad}</td>
-    <td>${precio}</td>
-    <td>
-    <button id="+${id}" class="btn-success">+</button>
-    <button id="-${id}" class="btn-danger">-</button>
-    </td> 
-    `;
-    listaCarrito.appendChild(body);
+//     let body = document.createElement("tr");
+//     body.className = "producto__carrito";
+//     body.innerHTML = `
+//     <th><img id="fotoProductoCarrito" src="${imagen}" class="card-image-top" style="width:100%; "</th>
+//     <td>${nombre}</td>
+//     <td>${cantidad}</td>
+//     <td>${precio / cantidad}</td>
+//     <td>${precio}</td>
+//     <td>
+//     <button id="+${id}" class="btn-success">+</button>
+//     <button id="-${id}" class="btn-danger">-</button>
+//     </td>
+//     `;
+//     listaCarrito.appendChild(body);
 
-    const btnAgregar = document.getElementById(`+${id}`);
-    const btnRestar = document.getElementById(`-${id}`);
+//     const btnAgregar = document.getElementById(`+${id}`);
+//     const btnRestar = document.getElementById(`-${id}`);
 
-    btnAgregar.addEventListener("click", () => aumentarCantidad(id));
-    btnRestar.addEventListener("click", () => restarCantidad(id));
-  });
+//     btnAgregar.addEventListener("click", () => aumentarCantidad(id));
+//     btnRestar.addEventListener("click", () => restarCantidad(id));
+//   });
 
-  hacerFooter();
-};
+//   hacerFooter();
+// };
 
-const hacerFooter = () => {
-  if (carrito.lenght > 0) {
-    footCarrito.innerHTML = "";
+// const hacerFooter = () => {
+//   if (carrito.lenght > 0) {
+//     footCarrito.innerHTML = "";
 
-    let footer = document.createElement("tr");
+//     let footer = document.createElement("tr");
 
-    footer.innerHTML = `
-    <th>Totales:</th>
-    <td></td>
-    <td>${generarTotales().cantidadTotal}</td>
-    <td></td>
-    <td>${generarTotales().costoTotal}</td>
-    `;
-    footCarrito.append(footer);
-  } else {
-    footCarrito.innerHTML = `<p>No hay productos en el carrito</p>`;
-  }
-};
+//     footer.innerHTML = `
+//     <th>Totales:</th>
+//     <td></td>
+//     <td>${generarTotales().cantidadTotal}</td>
+//     <td></td>
+//     <td>${generarTotales().costoTotal}</td>
+//     `;
+//     footCarrito.append(footer);
+//   } else {
+//     footCarrito.innerHTML = `<p>No hay productos en el carrito</p>`;
+//   }
+// };
 
-const generarTotales = () => {
-  const costoTotal = carrito.reduce(
-    (acumulador, { precio }) => total + precio,
-    0
-  );
-  const cantidadTotal = carrito.reduce(
-    (total, { cantidad }) => total + cantidad,
-    0
-  );
-  return {
-    costoTotal: costoTotal,
-    cantidadTotal: cantidadTotal,
-  };
-};
+// const generarTotales = () => {
+//   const costoTotal = carrito.reduce(
+//     (acumulador, { precio }) => total + precio,
+//     0
+//   );
+//   const cantidadTotal = carrito.reduce(
+//     (total, { cantidad }) => total + cantidad,
+//     0
+//   );
+//   return {
+//     costoTotal: costoTotal,
+//     cantidadTotal: cantidadTotal,
+//   };
+// };
 
-const aumentarCantidad = (id) => {
-  const indexProductoCarrito = carrito.findIndex(
-    (producto) => producto.id === id
-  );
-  const precio =
-    carrito[indexProductoCarrito].precio /
-    carrito[indexProductoCarrito].cantidad;
+// const aumentarCantidad = (id) => {
+//   const indexProductoCarrito = carrito.findIndex(
+//     (producto) => producto.id === id
+//   );
+//   const precio =
+//     carrito[indexProductoCarrito].precio /
+//     carrito[indexProductoCarrito].cantidad;
 
-  carrito[indexProductoCarrito].cantidad++;
-  carrito[indexProductoCarrito].precio =
-    precio * carrito[indexProductoCarrito].cantidad;
+//   carrito[indexProductoCarrito].cantidad++;
+//   carrito[indexProductoCarrito].precio =
+//     precio * carrito[indexProductoCarrito].cantidad;
 
-  sessionStorage.setItem("carrito", JSON.stringify(carrito));
+//   sessionStorage.setItem("carrito", JSON.stringify(carrito));
 
-  hacerCarrito();
-};
+//   hacerCarrito();
+// };
 
-const restarCantidad = (id) => {
-  const indexProductoCarrito = carrito.findIndex(
-    (producto) => producto.id === id
-  );
-  const precio =
-    carrito[indexProductoCarrito].precio /
-    carrito[indexProductoCarrito].cantidad;
+// const restarCantidad = (id) => {
+//   const indexProductoCarrito = carrito.findIndex(
+//     (producto) => producto.id === id
+//   );
+//   const precio =
+//     carrito[indexProductoCarrito].precio /
+//     carrito[indexProductoCarrito].cantidad;
 
-  carrito[indexProductoCarrito].cantidad--;
-  carrito[indexProductoCarrito].precio =
-    precio * carrito[indexProductoCarrito].cantidad;
-  if (carrito[indexProductoCarrito].cantidad === 0) {
-    carrito.splice(indexProductoCarrito, 1);
-  }
+//   carrito[indexProductoCarrito].cantidad--;
+//   carrito[indexProductoCarrito].precio =
+//     precio * carrito[indexProductoCarrito].cantidad;
+//   if (carrito[indexProductoCarrito].cantidad === 0) {
+//     carrito.splice(indexProductoCarrito, 1);
+//   }
 
-  sessionStorage.setItem("carrito", JSON.stringify(carrito));
+//   sessionStorage.setItem("carrito", JSON.stringify(carrito));
 
-  hacerCarrito();
-};
+//   hacerCarrito();
+// };
--- js/inicio.js
+++ js/inicio.js
@@ -1,54 +1,66 @@
-import { comprarProducto } from "./carrito.js";
-
+// import { comprarProducto } from "./carrito.js";
 const divProductos = document.getElementById("productos");
 const filterInput = document.getElementById("filter__input");
 
-export let productosDisponibles = JSON.parse(localStorage.getItem("productos"));
+// Traemos datos al DOM
+const traerDatos = async () => {
+  try {
+    const response = await fetch("./db/data.json");
+    const data = await response.json();
+    data.forEach((item) => {
+      const { nombre, categoria, precio, imagen, id } = item;
+      let card = document.createElement("div");
+      card.className = "producto";
+      card.innerHTML = `
+        <div class="card" style="width: 18rem;">
+      <img src="${imagen}" class="card-img-top" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">${nombre}</h5>
+        <p class="card-text">Categoria: ${categoria}</p>
 
-document.addEventListener("DOMContentLoaded", () => {
-  generarCardsProductos(productosDisponibles);
-});
-
-export const generarCardsProductos = (productos) => {
-  divProductos.innerHTML = "";
-
-  productos.forEach((producto) => {
-    const { nombre, categoria, precio, imagen, id } = producto;
-
-    let card = document.createElement("div");
-    card.className = "producto";
-    card.innerHTML = `
-    <div class="card" style="width: 18rem;">
-  <img src="${imagen}" class="card-img-top" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">${nombre}</h5>
-    <p class="card-text">Categoria: ${categoria}</p>
-
-    <p class="card-text">Precio: $${precio}</p>
-    <button id="comprar${id}" class="btn btn-primary">Comprar</button>
-    
-  </div>
-</div>`;
-
-    divProductos.appendChild(card);
-
-    const btnComprar = document.getElementById(`comprar${id}`);
-    btnComprar.addEventListener("click", () => comprarProducto(id));
-  });
+        <p class="card-text">Precio: $${precio}</p>
+        <button id="comprar${id}" class="btn btn-primary">Comprar</button>
+      </div>
+    </div>`;
+      divProductos.appendChild(card);
+      const btnComprar = document.getElementById(`comprar${id}`);
+      btnComprar.addEventListener("click", () => comprarProducto(id));
+    });
+  } catch (error) {
+    console.error("No se encontro la info", error);
+  }
 };
+traerDatos();
 
-// Filtrar por input
-filterInput.addEventListener("keyup", (e) => {
-  const productosFilter = productosDisponibles.filter((producto) =>
-    producto.nombre.toLowerCase().includes(e.target.value)
-  );
+// Filtrado Input
 
-  productosDisponibles = productosFilter;
+const saveKeyword = (keyword) => {
+  let keywords = JSON.parse(localStorage.getItem("keywords")) || [];
+  if (!keywords.includes(keyword)) {
+    keywords.unshift(keyword);
+    localStorage.setItem("keywords", JSON.stringify(keywords));
+  }
+};
+// filterInput.addEventListener("input", () => {});
 
-  if (e.target.value !== "") {
-    generarCardsProductos(productosFilter);
-  } else {
-    productosDisponibles = JSON.parse(localStorage.getItem("productos"));
-    generarCardsProductos(productosDisponibles);
+filterInput.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    const query = filterInput.value.toLowerCase();
+    saveKeyword(query);
   }
 });
+
+// filterInput.addEventListener("keyup", (e) => {
+//   const productosFilter = productosDisponibles.filter((producto) =>
+//     producto.nombre.toLowerCase().includes(e.target.value)
+//   );
+
+//   productosDisponibles = productosFilter;
+
+//   if (e.target.value !== "") {
+//     generarCardsProductos(productosFilter);
+//   } else {
+//     productosDisponibles = JSON.parse(localStorage.getItem("productos"));
+//     generarCardsProductos(productosDisponibles);
+//   }
+// });
